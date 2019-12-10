13ccadd797ca4c6e529ee6c79ca5f6026f6b40a7
Eve <major.eve.2426@gmail.com>
2019-12-10 09:52:17 -0500

    Calculator now fully functionnal, added commas

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,23 +12,78 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentValue = this.currentValue.toString().slice(0, -1)
     }
 
     addNumber(number) {
-        this.currentValue = number
+        if (number === '.' && this.currentValue.includes('.')) return
+        this.currentValue = this.currentValue.toString() + number.toString()
     }
 
     selectOperation(operation) {
-
+        if (this.currentValue === '') return
+        if (this.previousValue !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousValue = this.currentValue
+        this.currentValue = ''
     }
 
     compute() {
+        let calculation
+        const previous = parseFloat(this.previousValue)
+        const current = parseFloat(this.currentValue)
+        if (isNaN(previous) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                calculation = previous + current
+                break
+            case '-':
+                calculation = previous - current
+                break
+            case '*':
+                calculation = previous * current
+                break
+            case '÷':
+                calculation = previous / current
+                break
+            default:
+                return
+        }
+        this.currentValue = calculation
+        this.operation = undefined
+        this.previousValue = ''
+    }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerNumbers = parseFloat(stringNumber.split('.')[0])
+        const decimalNumbers = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerNumbers)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerNumbers.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalNumbers != null) {
+            return `${integerDisplay}.${decimalNumbers}`
+        } else {
+            return integerDisplay
+        }
     }
 
     updateDisplay() {
-        this.currentValueTxtElement.innerText = this.currentValue
+        this.currentValueTxtElement.innerText =
+            this.getDisplayNumber(this.currentValue)
+        if (this.operation != null) {
+            this.previousValueTxtElement.innerText =
+                `${this.getDisplayNumber(this.previousValue)} ${this.operation}`
+        } else {
+            this.previousValueTxtElement.innerText = ''
+        }
     }
 }
 // BUTTONS
@@ -48,4 +103,26 @@ numBtns.forEach(button => {
         calculator.addNumber(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.selectOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsBtn.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+clearBtn.addEventListener('click', button => {
+    calculator.clearAll()
+    calculator.updateDisplay()
+})
+
+delBtn.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
